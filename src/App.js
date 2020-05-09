a535690e1c291195cf912b5c0af684e31ca2fb4d
Lizzie <elizabethsiancrockett@gmail.com>
2020-05-09 23:08:17 +0100

    first playing around here

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+      <CountButton text="click meeeeeeee!!!!!1!" />
+      <InfoBar type="text" id="info" />
     </div>
   );
 }
+
+const myDate = new Date();
+
+const Heading = (props) => {
+  const time = myDate.getHours();
+  return (
+    <>
+      <h1>Good day to you.</h1>
+      <h2>The time is {time} hundred hours</h2>
+    </>
+  )
+}
+
+const CountButton = (props) => {
+  const [count, setCount] = React.useState(0);
+  return (
+    <>
+      <button onClick={() => setCount(count + 1)}>{props.text}</button>
+      <p>You have clicked the button {count} times</p>
+    </>
+  )
+}
+
+const InfoBar = (props) => {
+  const [info, displayInfo] = React.useState("write something in the box above");
+  const handleInput = (event) => {
+    displayInfo(event.target.value)
+  }
+  return (
+    <>
+      <input type={props.type} id={props.id} onChange={handleInput} />
+      <p>{info}</p>
+    </>
+  )
+}
+
 
 export default App;
