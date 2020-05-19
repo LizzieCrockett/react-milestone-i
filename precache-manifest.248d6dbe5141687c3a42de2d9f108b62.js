42fca48b319a61bca7ac268909a92b39f9915207
Lizzie <elizabethsiancrockett@gmail.com>
2020-05-19 18:49:24 +0100

    Updates

---
+++ precache-manifest.248d6dbe5141687c3a42de2d9f108b62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9444bfe857c20399640236526b7e1b51",
+    "url": "/react-milestone-i/index.html"
+  },
+  {
+    "revision": "3b31cdb16a7fe9f51227",
+    "url": "/react-milestone-i/static/css/main.65ad9d1b.chunk.css"
+  },
+  {
+    "revision": "fb2ac9780658c8754611",
+    "url": "/react-milestone-i/static/js/2.42558c37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-milestone-i/static/js/2.42558c37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b31cdb16a7fe9f51227",
+    "url": "/react-milestone-i/static/js/main.44e1691b.chunk.js"
+  },
+  {
+    "revision": "853d18f3451bf27f29c2",
+    "url": "/react-milestone-i/static/js/runtime-main.3b079d3e.js"
+  }
+]);
